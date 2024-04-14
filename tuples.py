bf9f143eefe08cd4ae910a1a9a1cfd3f53878e41
Margaret-Nyambura <margaretmacharianyambura@gmail.com>
2024-04-14 04:44:57 +0300

    added files

--- tuples.py
+++ tuples.py
@@ -1,7 +1,7 @@
 # tuples and how to access items from the tuple
 
 animals = ("Goat", "Cow", "Camel", "Donkey")
-
+print(animals)
 print(animals[2])
 print(animals[0 : 3])
 
@@ -17,3 +17,15 @@ print(animals2)
 # Removing item from a tuple
 print(animals2.pop())
 
+
+# finding length of a tuple
+print(len(animals))
+
+# printing every item in a tuple
+for i in animals : print(i)
+
+# membership check
+print(animals)
+print("Cow" in animals)
+
+print("Zebra" in animals)
